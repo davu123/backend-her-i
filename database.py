f3d0830ed6c4d48b6b3907e5ebb90e776e4ccc80
Davi Oliveira Maia <55770823+davu123@users.noreply.github.com>
2024-11-25 23:58:35 -0300

    Update database.py

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://usuario:senha@localhost:5432/nome_do_banco"
+DATABASE_URL = "postgresql://usuario:senha@localhost:5432/heroi-db.sql"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
