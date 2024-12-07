811514c77bb1fe439bf4346ebc04af5a57181d0f
Davi Oliveira Maia <55770823+davu123@users.noreply.github.com>
2024-12-07 02:27:16 -0300

    Update models.py

--- models.py
+++ models.py
@@ -1,9 +1,11 @@
 from sqlalchemy import Column, Integer, String
 from database import Base
 
-class Item(Base):
-    __tablename__ = "items"
+class Heroi(Base):
+    __tablename__ = "herois"
 
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, index=True)
     description = Column(String, nullable=True)
+    missao = Column(String, nullable=True)  # Nova coluna para missão
+    popularidade = Column(Integer, default=0)  # Nova coluna para popularidade
